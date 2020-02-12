aab12fae00a30681e00cf9ed9d4ee7f932c26817
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2020-02-12 14:11:39 +0100

    Added tests

---
+++ tests/fixtures/complete/plugins/ecommerce/settings/settings.py
@@ -0,0 +1,2 @@
+from ..derex import *
+
---
+++ tests/fixtures/complete/settings/base.py
@@ -0,0 +1,10 @@
+import os
+
+SERVICE_VARIANT = os.environ["SERVICE_VARIANT"]
+assert SERVICE_VARIANT in ("lms", "cms")
+exec(
+    "from {}.envs.derex.base import *".format(SERVICE_VARIANT), globals(), locals(),
+)
+
+# Id of the site fixture to use, instead of looking up the hostname
+SITE_ID = 1
